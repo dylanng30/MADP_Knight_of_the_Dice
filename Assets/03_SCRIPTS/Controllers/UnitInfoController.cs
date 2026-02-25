47aae775eb561cde3d122afabc114acccbc03d00
DylanNguyen3010 <dylan.ng3010@gmail.com>
2026-02-25 15:06:14 +0700

    Add UnitInfo

---
+++ Assets/03_SCRIPTS/Controllers/UnitInfoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MADP.Utilities;
+using MADP.Views;
+using MADP.Views.UnitInfo;
+using UnityEngine;
+
+namespace MADP.Controllers
+{
+    public class UnitInfoController : MonoBehaviour
+    {
+        [SerializeField] private UnitInfoView unitInfoView;
+
+        private void Awake()
+        {
+            unitInfoView.Clear();
+        }
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                OnMouseDown();
+            }
+        }
+
+        private void OnMouseDown()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            int cellLayer = LayerMask.GetMask(Constants.CellView);
+
+            if (Physics.Raycast(ray, out RaycastHit cellHit, 500, cellLayer))
+            {
+                var cellView = cellHit.collider.GetComponent<CellView>();
+                if (cellView != null && cellView.Model.HasUnit)
+                {
+                    var unitModel = cellView.Model.Unit;
+                    unitInfoView.Setup(unitModel);
+                }
+                else
+                {
+                    Clear();
+                }
+            }
+        }
+
+        private void Clear()
+        {
+            unitInfoView.Clear();
+        }
+    }
+}
---
+++ Assets/03_SCRIPTS/Views/UnitInfo/UnitInfoView.cs
@@ -0,0 +1,50 @@
+﻿using MADP.Models;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MADP.Views.UnitInfo
+{
+    public class UnitInfoView : MonoBehaviour
+    {
+        [SerializeField] private Image Avatar;
+
+        [Header("HEALTH")]
+        [SerializeField] private Image healthBar;
+        [SerializeField] private TextMeshProUGUI healthRatioTxt;
+
+        [Header("DAMAGE")]
+        [SerializeField] private TextMeshProUGUI damageTxt;
+
+        [Header("ARMOR")]
+        [SerializeField] private TextMeshProUGUI armorTxt;
+
+        private UnitModel unitModel;
+
+        public void Setup(UnitModel model)
+        {
+            unitModel = model;
+            healthBar.fillAmount = (float)unitModel.Stat.CurrentHealth / unitModel.Stat.MaxHealth;
+            healthRatioTxt.text = $"{unitModel.Stat.CurrentHealth}/{unitModel.Stat.MaxHealth}";
+            damageTxt.text = unitModel.Stat.Damage.ToString();
+            armorTxt.text = unitModel.Stat.Armor.ToString();
+
+            gameObject.SetActive(true);
+        }
+
+        public void Clear()
+        {
+            gameObject.SetActive(false);
+            unitModel = null;           
+        }
+
+        private void Update()
+        {
+           if(unitModel != null)
+           {
+                
+           }
+        }
+
+    }
+}
